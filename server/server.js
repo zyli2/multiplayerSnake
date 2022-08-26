1f2cb154269fe17604f6ffbbe0fdbb42a73436f4
zyli2 <zyli2@illinois.edu>
2022-08-26 01:59:47 -0500

    added cors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
-const io = require('socket.io')(server, { origins: '*:*'});
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "",
+    methods: ["GET", "POST"]
+  }
+});
 const { initGame, gameLoop, getUpdatedVelocity } = require('./game');
 const { FRAME_RATE } = require('./constants');
 const { makeid } = require('./utils');
