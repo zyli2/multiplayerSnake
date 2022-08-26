cd8db10f03c880834718a1203ded05ce9f52fc64
zyli2 <zyli2@illinois.edu>
2022-08-25 23:55:28 -0500

    added frontend

---
+++ frontend/index.js
@@ -0,0 +1,158 @@
+const BG_COLOR = '#231f20';
+const SNAKE_COLOR = '#c2c2c2';
+const FOOD_COLOR = '#e66916';
+
+const socket = io('http://localhost:3000');
+
+socket.on('init', handleInit);
+socket.on('gameState', handleGameState);
+socket.on('gameOver', handleGameOver);
+socket.on('gameCode', handleGameCode);
+socket.on('unknownGame'. handleUnknownGame);
+socket.on('tooManyPlayers'. handleTooManyPlayers);
+
+const gameScreen = document.getElementById('gameScreen');
+const initialScreen = document.getElementById('initialScreen');
+const newGameBtn = document.getElementById('newGameButton');
+const joinGameBtn = document.getElementById('joinGameButton');
+const gameCodeInput = document.getElementById('gameCodeInput');
+const gameCodeDisplay = document.getElementById('gameCodeDisplay');
+
+newGameBtn.addEventListener('click', newGame);
+joinGameBtn.addEventListener('click', joinGame);
+
+function newGame() {
+    socket.emit('newGame');
+    init();
+}
+
+function joinGame() {
+    const code = gameCodeInput.value;
+    socket.emit('joinGame', code);
+    init();
+}
+
+let canvas, ctx;
+let playerNumber;
+let gameActive = false;
+
+const gameState = {
+    player: {
+        pos: {
+            x: 3,
+            y: 10,
+        },
+        vel: {
+            x: 1,
+            y: 0,
+        },
+        snake: [
+            {x: 1, y: 10},
+            {x: 2, y: 10},
+            {x: 3, y: 10},
+        ],
+    },
+    food: {
+        x: 7,
+        y: 7,
+    },
+    // there are 20 squares in a grid
+    gridsize: 20,
+};
+
+function init() {
+    initialScreen.style.display = "none";
+    gameScreen.style.display = "block";
+    
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    canvas.width = canvas.height = 600;
+
+    ctx.fillStyle = BG_COLOR;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    document.addEventListener('keydown', keydown);
+    gameActive = true;
+}
+
+function keydown(e) {
+    socket.emit('keydown', e.keyCode);
+}
+
+function paintGame(state) {
+    ctx.fillStyle = BG_COLOR;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const food = state.food;
+    const gridsize = state.gridsize;
+    // calculates the pixels per grid square in game space
+    const size = canvas.width / gridsize;
+
+    ctx.fillStyle = FOOD_COLOR;
+    ctx.fillRect(food.x * size, food.y * size, size, size);
+
+    paintPlayer(state.players[0], size, SNAKE_COLOR);
+    paintPlayer(state.players[1], size, 'red');
+}
+
+function paintPlayer(playerState, size, color) {
+    const snake = playerState.snake;
+
+    ctx.fillStyle = color;
+    for (let cell of snake) {
+        ctx.fillRect(cell.x * size, cell.y * size, size, size);
+    }
+}
+
+function handleInit(number) {
+    playerNumber = number;
+    console.log(msg);
+}
+
+// now every time the server sends a gameState message with the new gameState object
+// the browser will receive it and it will paint the game using paintGame function
+function handleGameState(gameState) {
+    if (!gameActive) {
+        return;
+    }
+    gameState = JSON.parse(gameState);
+    requestAnimationFrame(() => paintGame(gameState));
+}
+
+function handleGameOver(data) {
+    if (!gameActive) {
+        return;
+    }
+    data = JSON.parse(data);
+
+    if (data.winner === playerNumber) {
+        alert("You've won the battle!");
+    } else {
+        alert('do not worry, there is always a next time!');
+    }
+    gameActive = false;
+}
+
+function handleGameCode(gameCode) {
+    gameCodeDisplay.innerText = gameCode;
+}
+
+function handleUnknownGame() {
+    reset();
+    alert("Unknown game code");
+}
+
+function handleTooManyPlayers() {
+    reset();
+    alert("This game is already in progress");
+}
+
+// resets the screen
+function reset() {
+    playerNumber = null;
+    gameCodeInput.value = "";
+    gameCodeDisplay.innerText = "";
+    initialScreen.style.display = "block";
+    gameScreen.style.display = "none";
+}
