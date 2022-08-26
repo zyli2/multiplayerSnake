962d3e1cfd21d5ed82bb2bd2f6cd1433a1a8e549
zyli2 <zyli2@illinois.edu>
2022-08-25 23:48:03 -0500

    uploaded all files

---
+++ server/constants.js
@@ -0,0 +1,9 @@
+// this file defines variables that allows us to access these variables anywhere in the server code
+
+const FRAME_RATE = 10;
+const GRID_SIZE = 20;
+
+module.exports = {
+    FRAME_RATE,
+    GRID_SIZE,
+}
